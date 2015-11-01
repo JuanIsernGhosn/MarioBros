1073bffe4bdbd884cab2de6e03c675a93bfea28a
JuanIsernGhosn <juan.isern95@gmail.com>
2015-11-01 19:52:32 +0000

    Initial commit

---
+++ core/src/com/pointers3/mariobros/Screens/PlayScreen.java
@@ -0,0 +1,69 @@
+package com.pointers3.mariobros.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.pointers3.mariobros.MarioBros;
+import com.pointers3.mariobros.Scenes.Hud;
+
+/**
+ * Created by juan on 01/11/2015.
+ */
+public class PlayScreen implements Screen {
+
+    private MarioBros game;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+    private Hud hud;
+
+    public PlayScreen(MarioBros game){
+        this.game = game;
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(MarioBros.V_WIDTH,MarioBros.V_HEIGHT,gamecam);
+        hud = new Hud(game.batch);
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ desktop/src/com/pointers3/mariobros/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pointers3.mariobros.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pointers3.mariobros.MarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBros(), config);
+	}
+}
